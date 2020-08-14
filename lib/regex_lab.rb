1c305bd63f1795df5e3f808dc2a6507b89b2992b
jchasse <jonathan.r.chasse@gmail.com>
2020-08-14 16:33:21 -0500

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,22 @@
-def starts_with_a_vowel?(word)
+require 'pry'
 
+def starts_with_a_vowel?(word)
+    !word[0].downcase.match(/[aeiou]/).nil?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
+    text.split.grep(/^un[\s\S]+ing$/)
 
 end
 
 def words_five_letters_long(text)
-
+    text.split.grep(/^\w{5}\z/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !text.match(/^[A-Z][\s\S]+[\.\!\?]$/).nil?
 end
 
 def valid_phone_number?(phone)
-
+    phone.scan(/[0-9]/).length == 10
 end
